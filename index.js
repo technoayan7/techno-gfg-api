28248814116e68f6625207d128e7ce9be7bc88e0
technoayan <ayanahmad7052@gmail.com>
2024-03-09 02:35:58 +0530

    Added GFG Api deploy to vercel

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const { Scrap } = require('./modules/scrap');
 
 const app = express();
 const port = 3000; 
+
+const limiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    limit: 90,
+    standardHeaders: "draft-7",
+    legacyHeaders: false,
+    message: "Too many request from this IP, try again in 1 hour",
+});
+
+app.use(cors()); //enable all CORS request
+app.use(limiter); //limit to all API
 
 app.get('/:username', async (req, res) => {
     const username = req.params.username;
