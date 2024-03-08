e8872340a8f8b25f722e145f6bbd7ccf940b31c8
technoayan <ayanahmad7052@gmail.com>
2024-03-09 00:49:03 +0530

    Added GFG Api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { Scrap } = require('./modules/scrap');
 
 const app = express();
+const port = 3000; 
 
 app.get('/:username', async (req, res) => {
     const username = req.params.username;
@@ -11,4 +12,6 @@ app.get('/:username', async (req, res) => {
     res.json(responseData);
 });
 
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
